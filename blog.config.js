8ca730acfc25f9fcbcf21a9219710e2e006238e7
Peng Yu Yan <37659181+wxyhgk@users.noreply.github.com>
2024-10-04 13:22:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '我心永恒', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '喜欢折腾的普通人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wxyhgk.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,wolfram,mathemtcia,化学,量子化学,gaussian,orca,ai,ChatGPT', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
